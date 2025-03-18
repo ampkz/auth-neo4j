d9b2be26323ebf77bead6993adcce80134e86b03
Andrew M. Pankratz <apankra@wgu.edu>
2025-03-18 12:53:15 -0400

    Rename users file

---
+++ src/routing/user.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import { sendStatus405 } from '../middleware/statusCodes';
+import { permitRoles } from '../middleware/permitRoles';
+import { Auth } from '../auth/auth';
+
+const router: Router = Router();
+
+router.get(`${process.env.AUTH_NEO4J_USER_URI as string}`, permitRoles(Auth.ADMIN));
+router.put(`${process.env.AUTH_NEO4J_USER_URI as string}`, sendStatus405('GET', 'POST'));
+router.delete(`${process.env.AUTH_NEO4J_USER_URI as string}`, sendStatus405('GET', 'POST'));
+// router.post(`${process.env.AUTH_NEO4J_USER_URI as string}`, permitRoles(Auth.ADMIN));
+
+export default router;
