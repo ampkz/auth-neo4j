e2588d0db1a6e0f78d4e23e686b1cd610662735c
Andrew M. Pankratz <apankra@wgu.edu>
2025-08-02 10:26:44 -0400

    Add entry points for api

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-	entry: ['src/index.ts'],
+	entry: ['src/index.ts', 'src/sessions/crud-session.ts', 'src/sessions/session.ts', 'src/users/user.ts', 'src/auth/auth.ts'],
 	format: ['cjs', 'esm'], // Build for commonJS and ESmodules
 	dts: true, // Generate declaration file (.d.ts)
 	splitting: false,
