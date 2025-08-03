6c107339527e4b728cea4b00551b3f56f0c6a45b
Andrew M. Pankratz <apankra@wgu.edu>
2025-08-03 09:16:59 -0400

    Refactor to allow db initialization and destruction

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,8 @@
 import { validateSessionToken } from './sessions/crud-session';
+import { initializeDB, destroyDB } from './db/utils';
 
 export default {
+	destroyDB,
+	initializeDB,
 	validateSessionToken,
 };
